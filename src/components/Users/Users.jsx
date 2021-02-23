2801e22498c597f147ae9783ac39373a8a49bae0
oleghmelka <oleghmelka@gmail.com>
2021-02-23 22:16:20 +0200

    add componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,8 +6,7 @@ import userPhoto from '../../assets/images/avatar.jpg';
 
 class Users extends React.Component {
 
-    constructor(props){
-        super(props);
+    componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => { this.props.setUsers(response.data.items) } );
     }
 
@@ -15,8 +14,7 @@ class Users extends React.Component {
         return(
             <div>
                 <h2>Users</h2>
-                
-
+            
                 {this.props.users.map( u => <div className={s.user} key={u.id}>
                     
                     <div className={s.userLeftInfo}>
