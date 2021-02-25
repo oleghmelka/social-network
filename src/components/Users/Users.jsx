daf8c3eee16a1162ba47ebe0dd38355da8b2d80f
oleghmelka <oleghmelka@gmail.com>
2021-02-25 14:36:51 +0200

    add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,14 +7,37 @@ import userPhoto from '../../assets/images/avatar.jpg';
 class Users extends React.Component {
 
     componentDidMount(){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => { this.props.setUsers(response.data.items) } );
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then( response => { 
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        } );
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then( response => { 
+            this.props.setUsers(response.data.items);
+        } );
     }
 
     render () {
+
+       
+        let pagesCount = Math.ceil( this.props.totalUsersCount / this.props.pageSize );
+        let pages = [];
+        for (let index = 1; index <= pagesCount; index++) {
+            pages.push(index);
+        }
+
         return(
             <div>
                 <h2>Users</h2>
             
+                <div className={s.pagination}>
+                    {pages.map( p => <span onClick={ (e) => {this.onPageChanged(p)} } className={this.props.currentPage === p && s.selectedPage}>{p}</span>) }
+                </div>
+
+
                 {this.props.users.map( u => <div className={s.user} key={u.id}>
                     
                     <div className={s.userLeftInfo}>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,10 +1,16 @@
 
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
-const SET_USERS = 'SET_USERS'
+const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_USERS_TOTAL_COUNT = 'SET_USERS_TOTAL_COUNT';
+
 
 let initialState = {
-    users: [] 
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1 
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -33,7 +39,13 @@ const usersReducer = (state = initialState, action) => {
 
             };
         case SET_USERS: {
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
+        }
+        case SET_CURRENT_PAGE: {
+            return { ...state, currentPage: action.currentPage }
+        }
+        case SET_USERS_TOTAL_COUNT: {
+            return { ...state, totalUsersCount: action.totalCount }
         }
         default:
             return state;
@@ -61,6 +73,23 @@ export const setUsersAC = (users) => {
         users: users
     }
 }
+
+export const setCurrentPageAC = (currentPage) => {
+    return { 
+        type: SET_CURRENT_PAGE,
+        currentPage: currentPage
+    }
+}
+
+export const setUsersTotalCountAC = (totalCount) => {
+    return { 
+        type: SET_USERS_TOTAL_COUNT,
+        totalCount: totalCount
+    }
+}
+ 
+
+
 
 
 
