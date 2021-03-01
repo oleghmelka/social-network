5400536a30c1cb3aa612089196fc49489f756caf
oleghmelka <oleghmelka@gmail.com>
2021-03-01 22:01:16 +0200

    stady withRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,14 +1,21 @@
 import * as axios from 'axios';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { setUserProfile } from '../../redux/profile-reducer';
 import Profile from './Profile';
 
 
 class ProfileContainer extends React.Component {
-    
+  
     componentDidMount(){
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+        
+        let userId = this.props.match.params.userId;
+        console.log(this.props.profile);
+        if (!userId) {
+            userId = 2;
+        }
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then( response => { 
                 this.props.setUserProfile(response.data);
         } );
@@ -28,8 +35,8 @@ class ProfileContainer extends React.Component {
  let mapStateToProps = (state) => ({
         profile: state.profilePage.profile
   }); 
-  console.log(window.store.getState());
   
+  
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-
-export default connect(mapStateToProps, {setUserProfile} ) (ProfileContainer);
+export default connect(mapStateToProps, {setUserProfile} ) (WithUrlDataContainerComponent);
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -37,7 +37,7 @@ const profileReducer = (state = initialState, action) => {
             }
         }
         case SET_USER_PROFILE: {
-            debugger;
+            
             return {...state, profile: action.profile}
         }
         default:
