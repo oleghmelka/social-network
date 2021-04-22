bd7d214382389fd604a47ecf27a29e6a74d3bf99
oleghmelka <oleghmelka@gmail.com>
2021-04-22 15:23:51 +0300

    i have problems with instalation react-test-renderer

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+//import { render, screen } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SocialNetworkApp from './App';
 
 /* test('renders learn react link', () => {
     render(<App />);
@@ -12,6 +12,6 @@ import ReactDOM from 'react-dom';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<SocialNetworkApp />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
